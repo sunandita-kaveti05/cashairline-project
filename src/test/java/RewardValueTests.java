dbfa10f38fa9071ca6cadd0edef65cb45c45b140
sunandita-kaveti05 <sun13rao@gmail.com>
2023-12-20 18:16:15 +0530

    first change

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -1,30 +1,46 @@
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class RewardValueTests {
+
+public class RewardValueTests  {
 
     @Test
     void create_with_cash_value() {
-        double cashValue = 100;
+        
+
+        double cashValue = 100.0;
         RewardValue rewardValue = new RewardValue(cashValue);
+        int expectedMiles = (int) (cashValue / 0.0035);
         assertEquals(cashValue, rewardValue.getCashValue());
     }
 
     @Test
     void create_with_miles_value() {
+    
+
         int milesValue = 10000;
         RewardValue rewardValue = new RewardValue(milesValue);
+        double expectedCash = milesValue * 0.0035;
         assertEquals(milesValue, rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+       assert false;
+
+        double cashValue = 100;
+        int expectedMilesValue = (int) (cashValue / 0.0035);
+        RewardValue rewardValue = new RewardValue(cashValue);
+        assertEquals(expectedMilesValue, rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+       assert false;
+
+        int milesValue = 10000;
+        double expectedCashValue = milesValue * 0.0035;
+        RewardValue rewardValue = new RewardValue(milesValue);
+        assertEquals(expectedCashValue, rewardValue.getCashValue());
     }
 }
